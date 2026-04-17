6522821b0abf4f172c920415e5972a758ac20f36
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-17 00:02:20 -0500

    velocidad y caida

--- limones.js
+++ limones.js
@@ -13,8 +13,10 @@ let limonX=canvas.width/2;
 let limonY=0;
 let puntaje=0;
 let vidas=3;
+let velocidadCaida=200;
 
 function iniciar(){
+    intervalo = setInterval(bajarLimon, velocidadCaida);
     dibujarSuelo();
     dibujarPersonaje();
     dibujarLimon();
